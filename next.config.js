6068a748c8d8a5e25ce3e7f2be10346aa8c92038
Naufal Akbar <kbrnugroho@gmail.com>
2022-06-18 13:43:15 +0700

    installing dev depedencies

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withImages = require('next-images')
+const nextConfig = withImages({
   reactStrictMode: true,
-}
+  exportTrailingSlash: true,
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+      '/profile': { page: '/profile' },
+      '/projects': { page: '/projects' },
+      '/contact': { page: '/contact' },
+      '/config': { page: '/config' },
+      '/socialfeeds': { page: '/socialfeeds' },
+    };
+  }
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
