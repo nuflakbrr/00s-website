2c746037b325fd8d16a9098f417cab0ed7a08112
Naufal Akbar <kbrnugroho@gmail.com>
2022-06-18 13:52:40 +0700

    try again

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  exportTrailingSlash: true,
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' },
-      '/profile': { page: '/profile' },
-      '/projects': { page: '/projects' },
-      '/contact': { page: '/contact' },
-      '/config': { page: '/config' },
-      '/socialfeeds': { page: '/socialfeeds' },
-    };
-  }
 }
 
 module.exports = nextConfig
