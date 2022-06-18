4c1bdd8897f33ec0ca758cd60153012161ff3ce0
Naufal Akbar <kbrnugroho@gmail.com>
2022-06-18 23:08:56 +0700

    starting write page index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,9 +11,11 @@ const Home: NextPage = () => {
         <Head>
           <title>Naufal Akbar Nugroho</title>
         </Head>
+
         <h1 className="blink">
           Hello world!
         </h1>
+
         <div className="marquee">
           <div className="marquee-content">
             <h4 className="marquee-item">
@@ -21,7 +23,11 @@ const Home: NextPage = () => {
             </h4>
           </div>
         </div>
-        <p>hello</p>
+
+        <p>Hello, welcome to my retro website. The reason why I made this website is because I miss the look of the website in the 2000s even though I was born in 2004, but I still had time to feel the look of the website around the 2000s hehehe.</p>
+
+        <p>I thank you very much for taking the time to visit this website. A little story that when I created this website I had a lot of trouble finding references for how to make a website look in the 2000s. I say many thanks to <a href="https://github.com/nauvalazhar" target={"_blank"} rel="noreferrer" className="author">Naufal Azhar</a> who created the layout for this website.</p>
+
         <p>Regards,<br /><b>Naufal Akbar Nugroho</b></p>
       </Main>
     </>
