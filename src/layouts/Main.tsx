f23f961a2ff69cc12a963160a782b8c5461eb651
Naufal Akbar <kbrnugroho@gmail.com>
2022-06-18 11:21:00 +0700

    fix marquee

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Head from 'next/head';
-import Link from 'next/link';
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
 
 type layoutProps = {
     children: React.ReactNode;
@@ -15,7 +15,7 @@ const Main = (props: layoutProps) => {
                 <meta name="google-site-verification" content="IoqwXa0jgdLrvXiqTt2gVyPCpMd80szFRn1cLEp8Ul4" />
                 <link rel="icon" href="/favicon-org.ico" />
             </Head>
-            <table width={800} border={4} cellPadding={20} className='border-4'>
+            <table width={800} border={4} cellPadding={20} className="border-4">
                 <thead>
                     <tr>
                         <th colSpan={2}>
@@ -34,10 +34,10 @@ const Main = (props: layoutProps) => {
                 </thead>
                 <tbody>
                     <tr>
-                        <td valign='top'>
+                        <td valign="top">
                             {props.children}
                         </td>
-                        <td width={250} valign='top'>
+                        <td width={250} valign="top">
                             {/* <img src={imgAvatar} width="100" /> */}
                             <p>Koala on Windows 7 is my favorite photo in the universe.</p>
                             <Link href="/profile"><a>See my profile</a></Link>
@@ -62,10 +62,10 @@ const Main = (props: layoutProps) => {
                 </tbody>
                 <tfoot>
                     <tr>
-                        <td colSpan={2} align='center'>
+                        <td colSpan={2} align="center">
                             <p>&copy; {year} <br /><br /></p>
                             <small>
-                                Design by <a href="https://github.com/nauvalazhar" target={'_blank'} rel="noreferrer" className="author">Nauval</a>
+                                Design by <a href="https://github.com/nauvalazhar" target={"_blank"} rel="noreferrer" className="author">Nauval</a>
                             </small>
                         </td>
                     </tr>
@@ -83,6 +83,22 @@ const Main = (props: layoutProps) => {
 				margin-bottom: 50px;
 				border-collapse: collapse;
 			}
+            // marquee
+            .marquee{
+                width: 100%;
+                background-color: red;
+                color: white;
+                text-transform: uppercase;
+                white-space: nowrap;
+                overflow: hidden;
+            }
+            .marquee-content{
+                display: inline-block;
+                animation: scrolling 5s linear infinite;
+            }
+            @keyframes scrolling {
+                100% { transform: translate(100%, 0); }
+            }
 			// blink polyfill
 			.blink {
 			  -webkit-animation: 1s linear infinite condemned_blink_effect; // for Safari 4.0 - 8.0
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/no-unescaped-entities */
-import type { NextPage } from 'next';
-import Head from 'next/head';
+import type { NextPage } from "next";
+import Head from "next/head";
 
-import Main from '../layouts/Main';
+import Main from "../layouts/Main";
 
 const Home: NextPage = () => {
   return (
@@ -11,17 +11,22 @@ const Home: NextPage = () => {
         <Head>
           <title>Naufal Akbar Nugroho</title>
         </Head>
-        <h1 className="text-3xl font-bold underline">
+        <h1 className="blink">
           Hello world!
         </h1>
-        <h4>
+        <div className="marquee">
+          <div className="marquee-content">
+            <h4 className="marquee-item">
+              Time to bring back the old school website brow
+            </h4>
+          </div>
           {/* <marquee>
             Time to bring back the old school website brow
           </marquee> */}
-        </h4>
+        </div>
         <p>Hello, this is my website. You can get to know me through this website. These days the website interface design varies greatly, from minimal design to complex design. Because of the large number of website design choices that make me confused to use what kind of style, eventually I decided to go back to 2000s website design.</p>
-        <p>The appearance of this website is also what I made when I first learned to make a web page using only HTML and also Notepad. But, because this is 2020 and there are lots of tools that make it easy for us to build a modern website, so I decided to build this website using Node, React/Next and Now; so that people don't think that I'm standing by obsolescence.</p>
-        <p>As I mentioned before, today we are facilitated by modern tools, such as Node, Tailwind, React, Vue, Gatsby, Webpack, and so on. Because of these tools, sometimes someone thinks creating a website today is complicated:  because we have to setup "this and that"; maybe it's true. On the other hand, I just want to "prove" that making website is easy, "these tools" are literally tools. So, without these tools we can still create a website. Also, when we need these tools, use them. Simple isn't it?</p>
+        <p>The appearance of this website is also what I made when I first learned to make a web page using only HTML and also Notepad. But, because this is 2020 and there are lots of tools that make it easy for us to build a modern website, so I decided to build this website using Node, React/Next and Now; so that people don"t think that I"m standing by obsolescence.</p>
+        <p>As I mentioned before, today we are facilitated by modern tools, such as Node, Tailwind, React, Vue, Gatsby, Webpack, and so on. Because of these tools, sometimes someone thinks creating a website today is complicated:  because we have to setup "this and that"; maybe it"s true. On the other hand, I just want to "prove" that making website is easy, "these tools" are literally tools. So, without these tools we can still create a website. Also, when we need these tools, use them. Simple isn"t it?</p>
         <p>The most important thing is that I am just an ordinary human whose every statement is potentially wrong - just like you.</p>
         <p>Regards,<br /><b>Nauval</b></p>
       </Main>
