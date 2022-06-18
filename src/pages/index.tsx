32c1aab601ba6e81253856e1e9c1cd0852932227
Naufal Akbar <kbrnugroho@gmail.com>
2022-06-18 23:33:50 +0700

    piw piw

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ const Home: NextPage = () => {
 
         <p>Hello, welcome to my retro website. The reason why I made this website is because I miss the look of the website in the 2000s even though I was born in 2004, but I still had time to feel the look of the website around the 2000s hehehe.</p>
 
-        <p>I thank you very much for taking the time to visit this website. A little story that when I created this website I had a lot of trouble finding references for how to make a website look in the 2000s. I say many thanks to <a href="https://github.com/nauvalazhar" target={"_blank"} rel="noreferrer" className="author">Naufal Azhar</a> who created the layout for this website.</p>
+        <p>I thank you very much for taking the time to visit this website. A little story that when I created this website I had a lot of trouble finding references for how to make a website look in the 2000s. I say many thanks to <a href="https://github.com/nauvalazhar" target={"_blank"} rel="noreferrer" className="link">Naufal Azhar</a> who created the layout for this website.</p>
 
         <p>Regards,<br /><b>Naufal Akbar Nugroho</b></p>
       </Main>
